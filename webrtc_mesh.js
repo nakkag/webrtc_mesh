c9fee19281c8540d929cfaddc1a44bcf71c64521
nakka <nakka@nakka.com>
2022-11-17 13:50:02 +0900

    エラー処理の改善

--- webrtc_mesh.js
+++ webrtc_mesh.js
@@ -46,8 +46,8 @@ function startVideo(roomId, localId) {
 			window.stream = stream;
 			localVideo.srcObject = stream;
 			startServerConnection(roomId, localId);
-		}).catch(e => {
-			alert('Camera start error.\n\n' + e.name + ': ' + e.message);
+		}).catch(error => {
+			alert('Camera start error.\n\n' + error.name + ': ' + error.message);
 		});
 	} else {
 		alert('Your browser does not support getUserMedia API');
@@ -186,7 +186,7 @@ function gotMessageFromServer(message) {
 	if (signal.ice) {
 		// ICE受信
 		if (pc.remoteDescription) {
-			pc.addIceCandidate(new RTCIceCandidate(signal.ice)).catch(function(error) {
+			pc.addIceCandidate(new RTCIceCandidate(signal.ice)).catch(error => {
 				pc._queue = new Array();
 				errorHandler(error);
 			});
